c9c7ccf076ca80a306d8a005e16db8a095cde2d4
APIVerve Team <hello@apiverve.com>
2024-05-14 15:29:50 -0500

    Bug fixes and Improvements

---
+++ lovecalculatorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class lovecalculatorQueryOptions {
+public string name1 { get; set; }
+public string name2 { get; set; }
+}
+
+}
